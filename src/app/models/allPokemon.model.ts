a38afac4185c160cb6543b766c00e23662145163
Carlos <carlosanzmunoz@gmail.com>
2024-12-11 13:32:23 +0100

    modelos de datos añadidos

---
+++ src/app/models/allPokemon.model.ts
@@ -0,0 +1,10 @@
+export interface IAllPokemon{
+    count:    number;
+    next:     string | null;
+    previous: string | null;
+    results:  Result[];
+}
+export interface Result {
+    name: string;
+    url:  string;
+}
